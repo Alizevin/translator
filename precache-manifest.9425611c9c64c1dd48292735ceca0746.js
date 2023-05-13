c80e11d9ea7545e4738ae8c5e33679843cb6fa5e
Alizevin <126564031+Alizevin@users.noreply.github.com>
2023-05-13 18:59:34 +0300

    Add files via upload

---
+++ precache-manifest.9425611c9c64c1dd48292735ceca0746.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a82218d1a74bab2689ab168b56d9008",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b0f9ea02dbae6cda432",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "3a37c87d825212c9acf3",
+    "url": "/static/js/2.00fb7832.chunk.js"
+  },
+  {
+    "revision": "3b0f9ea02dbae6cda432",
+    "url": "/static/js/main.9519a10e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
